cd29e05ee7a14e7352428a07b4bf6390c6ead914
nkevy <yvekhaon@gmail.com>
2020-10-29 11:17:31 -0700

    ajax note

--- ajax_exmpl.js
+++ ajax_exmpl.js
@@ -4,13 +4,34 @@ var request = new XMLHttPRquest();
 //use post for local host backend call
 request.open()
 request.onreadystatechange=function(){
+//
 // 0 unsent
 // 1 open 
 // 2 headers_received
 // 3 loading
 // 4 done
+//
+// 200 ok
+// 404 not found
+// 503 unavailable (no connection)
+//
 	if (this.readyState===4 && this.status===200){
-		return this.responseText;
+		// here add to state redux store
+		// dispatch an action with formatted text as responce
+		var resp = this.responseText;
+		// is a json format 
+		// parse with JSON.parse('json',function(key,value){
+			if (key=="sucsess"){
+				return Boolean(value)
+			}else{
+				return value
+			}
+		})
+		if (resp.succsess){
+			//dispatch action with error code
+		}else{
+			//dispatch action with payload
+		}
 	}
 }
 request.send()
